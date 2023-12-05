882888ad84f313158f8c8de0acd31507ea386cc8
Brent Smith <brentosmith@gmail.com>
2023-12-04 22:33:17 -0600

    replace social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
 
 	themeConfig: {
 		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
+		image: "img/voyager.png",
 		colorMode: {
 			defaultMode: "dark",
 		},
